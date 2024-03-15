c9d60a4b5c7367120041bbf6b6a4a2614fbc2b61
Khaleed <158002985+khaleedj3@users.noreply.github.com>
2024-03-15 12:57:44 +0000

    Update FakeNews.js

--- FakeNews.js
+++ FakeNews.js
@@ -5,3 +5,20 @@ textarea.addEventListener("keyup", e =>{
     let scHeight = e.target.scrollHeight;
     textarea.style.height = `${scHeight}px`;
 });
+
+
+let checkbox = document.getElementById("check");
+var darkgradient = 'linear-gradient(#272727, #181818)'; 
+var lightgradient = 'linear-gradient(#e3e3e3, #dcdbdb)';
+
+function isChecked(){
+    if(checkbox.checked == 1){
+       document.body.style.background = lightgradient;
+       document.getElementById("lightmodetext").textContent = " Light Mode ";
+       document.getElementById("lightmodetext").style.color = "black";
+    }else{
+        document.body.style.background = darkgradient;
+        document.getElementById("lightmodetext").textContent = "Dark Mode";
+        document.getElementById("lightmodetext").style.color = "white";
+    }
+}
