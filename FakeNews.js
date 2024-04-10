c6973f6b371882de96e6e7eec12a7b854107f361
Ashes <125026164+Ashes05@users.noreply.github.com>
2024-04-10 23:40:32 +0100

    Update FakeNews.js

--- FakeNews.js
+++ FakeNews.js
@@ -1,15 +1,21 @@
-// Selects the textarea from the page
-const textarea = document.querySelector("textarea");
-
-// Adds an event listener to the textare
-textarea.addEventListener("keyup", e =>{
-    // Sets the initial height of the textarea to 400px
-    textarea.style.height = "400px";
-    // Get the height of the content in the textarea
-    let scHeight = e.target.scrollHeight;
-    // Set the textarea to the scroll height, allowing it to grow as the user inputs content
-    textarea.style.height = `${scHeight}px`;
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Selects the textarea from the page
+    const textarea = document.querySelector("textarea");
+    if (textarea) {
+        // Adds an event listener to the textarea
+        textarea.addEventListener("keyup", e => {
+        // Sets the initial height of the textarea to 400px
+        textarea.style.height = "400px";
+        // Get the height of the content in the textarea
+        let scHeight = e.target.scrollHeight;
+        // Set the textarea to the scroll height, allowing it to grow as the user inputs content
+        textarea.style.height = `${scHeight}px`;
+        });
+    } else {
+        console.error('Textarea element not found');
+    }
 });
+
 
 // Selects the checkbox from the page
 let checkbox = document.getElementById("check");
@@ -20,7 +26,7 @@ var lightgradient = 'linear-gradient(#e3e3e3, #dcdbdb)';
 
 // Function to toggle between the light and dark mode
 function isChecked(){
-    //check if the check box it checked
+    //check if the check box is checked
     if(checkbox.checked == 1){
         // Set the bodys background to the light gradient
        document.body.style.background = lightgradient;
