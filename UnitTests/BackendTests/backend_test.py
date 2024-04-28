65414b5dce51164fda568b8219754ec8849296cd
Andrew8833 <102183381+Andrew8833@users.noreply.github.com>
2024-04-28 22:51:58 +0100

    Add files via upload

---
+++ UnitTests/BackendTests/backend_test.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch
+from app import app, prediction
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please copy artical here', response.data)
+
+    @patch('builtins.print')
+    def test_prediction(self, mock_print):
+        test_text = "Hello, there folks today we are going to contribute contribution contributed like likes likely liked i dont care anymore!!!"
+        expected_output = ['hello', 'folk', 'today', 'go', 'contribut', 'contribut', 'contribut', 'like', 'like', 'like', 'like', 'dont', 'care', 'anymor']
+        prediction(test_text)
+        mock_print.assert_called_with(expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
